884224eee954b98bf0ff58c184c2b24f18daaad7
juclqs <juclqs@gmail.com>
2025-05-04 21:49:36 -0300

    atualização

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -8,9 +8,15 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display_links = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
 
+
+class MenuLinkInline(admin.StackedInline):
+    model = MenuLink
+    extra = 1
+
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description',
+    inlines = MenuLinkInline,
  
     def has_add_permission(self, request):
         return not SiteSetup.objects.exists()
